cc468ea9d988d28be4bc04d239eb4c16aa21c797
nhommm9 <94337789+nhommm9@users.noreply.github.com>
2021-11-20 23:23:05 +0700

    Delete Public directory

--- Public/js/app.js
+++
@@ -1,8 +0,0 @@
-< script src = "https://code.jquery.com/jquery-3.6.0.js" > < /script>
-
-<
-script >
-    $(document).ready(function() {
-        $('.form-chon-sub-menu').parent('a').addClass('has-child')
-    }); <
-/script>
